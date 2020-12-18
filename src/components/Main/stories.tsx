a1ef458bb20b6185212a73895998aca815c15ff6
Junior <jose@vert.se>
2020-12-18 19:26:25 -0300

    correção do eslint

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,13 +1,13 @@
-import {Story, Meta} from '@storybook/react/types-6-0'
+import { Story, Meta } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
-    title: 'Main',
-    component: Main
+  title: 'Main',
+  component: Main
 } as Meta
 
 export const Basic: Story = (args) => <Main {...args} />
 Basic.args = {
-    title: 'Titulo',
-    description: 'Teste descrição'
+  title: 'Titulo',
+  description: 'Teste descrição'
 }
